7926d7423468c28a932a0496aed34f33ab55bfc6
Tshidis0 <tshidisorasemetse7@gmail.com>
2024-09-11 12:53:45 +0000

    Done

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,7 @@
-import {fromJS } from 'immutable';
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    const immutableObject = fromJS(object);
-    return immutableObject.getIn(array);
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
 }
--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,16 +1,24 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(grades) {
-  const seq = Seq(grades);
-  const students = seq.filter((val) => val.score > 70).toJS();
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-  Object.keys(students).map((key) => {
-    const name = students[key].firstName;
-    const surname = students[key].lastName;
-
-    students[key].firstName = name.charAt(0).toUpperCase() + name.slice(1);
-    students[key].lastName = surname.charAt(0).toUpperCase() + surname.slice(1);
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
   });
 
-  console.log(students);
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
